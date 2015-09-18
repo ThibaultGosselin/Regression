59047f1089d6c07af9ebe960d37aa86898a3b2a0
ThibaultGosselin <thibault.gosselin@live.fr>
2015-09-18 09:41:25 +0200

    Update droite.py

--- droite.py
+++ droite.py
@@ -5,7 +5,7 @@ x=10*np.random.random(25)
 
 def linear(x,params=(0,1)):
 
-	return params[1]*x+params[0]+N(0,1)
+	return params[1]*x+params[0]+5*np.random.normal(size=len(x)) """droite+bruit distribué de selon une Gaussienne"""
 
 
 print linear(x, (3,10))
